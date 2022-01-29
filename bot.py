9eb8d37977f4661761cc335473e6332fe8be3a89
ByteSore <bytesore@tuxdoos.lan>
2022-01-29 10:45:02 +0100

    Changed return messages

--- bot.py
+++ bot.py
@@ -19,10 +19,10 @@ async def check(ctx, wallet: str):
         check_data = psql.selectone("SELECT address FROM v2_addresses WHERE address = %s", (wallet,))
 
         if check_data is None or check_data is []:
-            await ctx.respond('<@{0.author.id}>, could not find your wallet.'.format(ctx), ephemeral=True)
+            await ctx.respond('Sorry, <@{0.author.id}>... you aren\'t whitelisted (yet!)'.format(ctx), ephemeral=True)
             #await ctx.respond('<@{0.author.id}>, could not find your wallet. Please use !register to save it.'.format(ctx), ephemeral=True)
         else:
-            await ctx.respond('<@{0.author.id}>, your wallet is stored in our database.'.format(ctx), ephemeral=True)
+            await ctx.respond('Congratulations <@{0.author.id}>! You are on the whitelist!'.format(ctx), ephemeral=True)
     except Exception:
         logging.error("error: ")
 
